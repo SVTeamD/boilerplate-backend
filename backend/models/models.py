0158d91e264fed525069b16335b9075a304d0891
juwon5272 <jackson5272@naver.com>
2022-07-14 15:39:41 +0900

    feat: make order menu database

--- backend/models/models.py
+++ backend/models/models.py
@@ -25,11 +25,21 @@ class Item(Base):
 
     owner = relationship("User", back_populates="items")
 
-class Menu(Base): # 메뉴 테이블 생성
+
+class Menu(Base):  # 메뉴 테이블 생성
     __tablename__ = "menus"
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(255), index=True)
     cost = Column(Integer)
     photo_url = Column(String(2083))
-    
+
+
+class Orders(Base):  # 메뉴 주문하기
+    __tablename__ = "orders"
+
+    customer_id = Column(Integer, primary_key=True, index=True)  # PK
+    store_id = Column(Integer, primary_key=True, index=True)  # PK
+    order_datetime = Column(String(255))
+    order_is_takeout = Column(Boolean)
+    order_cost = Column(Integer)
