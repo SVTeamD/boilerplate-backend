f70b6cfbe42711c08f395eed5bf0a46c81aef274
juwon5272 <jackson5272@naver.com>
2022-07-18 22:33:31 +0900

    feat: order_api schemas

--- backend/models/schemas.py
+++ backend/models/schemas.py
@@ -1,4 +1,5 @@
 from typing import List, Union
+from xmlrpc.client import boolean
 
 from pydantic import BaseModel
 
@@ -35,3 +36,41 @@ class User(UserBase):
 
     class Config:
         orm_mode = True
+
+
+# juwon
+class OrderBase(BaseModel):
+    order_id: int
+    customer_id: int
+    store_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class Order(OrderBase):
+    order_datetime: int
+    order_is_takeout: boolean
+    order_cost: int
+
+
+class OrderCreate(Order):
+    pass
+
+
+class OrderRead(Order):
+    pass
+
+
+class OrderDelete(Order):
+    pass
+
+# class Order(Base):  # 메뉴 주문하기
+#     __tablename__ = "orders"
+
+#     order_id = Column(Integer, primary_key=True, index=True)  # PK
+#     customer_id = Column(Integer, ForeignKey("customer_id"), index=True)  # FK1
+#     store_id = Column(Integer, ForeignKey("store_id"), index=True)  # FK2
+#     order_datetime = Column(Integer)
+#     order_is_takeout = Column(Boolean)
+#     order_cost = Column(Integer)
